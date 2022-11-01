fa0e6fa4387be407e0b1633447ea397f938e1596
Kubanych Asanov <kuba.asanov4@gmail.com>
2022-11-01 16:16:47 +0600

    Configured jdbs for connection with DB.

--- src/main/java/com/example/banksystem/BankSystemApplication.java
+++ src/main/java/com/example/banksystem/BankSystemApplication.java
@@ -2,8 +2,9 @@ package com.example.banksystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BankSystemApplication {
 
 	public static void main(String[] args) {
