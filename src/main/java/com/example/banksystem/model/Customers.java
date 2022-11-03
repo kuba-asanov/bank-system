a770755114d8d54b5555d236b833ae2374152099
Kubanych Asanov <kuba.asanov4@gmail.com>
2022-11-03 15:32:28 +0600

    Implemented Customer model.

---
+++ src/main/java/com/example/banksystem/model/Customers.java
@@ -0,0 +1,62 @@
+package com.example.banksystem.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@Component
+@Document(collection="Customers")
+public class Customers {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String mobile;
+    private int balance;
+
+    public Customers() {
+
+    }
+
+    public Customers(String id, String name, String email, String mobile, int balance) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+        this.balance = balance;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
+
